d0d9775e6c05328ac1cfeb57c19e255c9dacc186
Tomkoid <tomaszierl@outlook.com>
2024-12-21 22:50:56 +0100

    chore: reuse the random function
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- src/game.cpp
+++ src/game.cpp
@@ -9,21 +9,21 @@
 
 Game::Game() {
   // prefill guesses
-  m_guesses.resize(max_attempts);
+  m_guesses.resize(MAX_ATTEMPTS);
   std::fill(m_guesses.begin(), m_guesses.end(), std::nullopt);
 
-  m_correct = get_random_num();
+  m_correct = get_random_num((int)MIN_NUM, (int)MAX_NUM);
 }
 
 void Game::init() {
-  std::cout << "I'm thinking of a number between " << min << " and " << max
-            << std::endl;
-  std::cout << "Your goal is to guess what is the number in " << max_attempts
+  std::cout << "I'm thinking of a number between " << MIN_NUM << " and "
+            << MAX_NUM << std::endl;
+  std::cout << "Your goal is to guess what is the number in " << MAX_ATTEMPTS
             << " attempts" << std::endl;
 }
 
 void Game::loop() {
-  for (int attempt = 0; attempt < max_attempts; attempt++) {
+  for (int attempt = 0; attempt < MAX_ATTEMPTS; attempt++) {
     int guess;
 
     try {
@@ -90,7 +90,7 @@ int Game::ask_user_guess() {
   return std::stoi(guess_str);
 }
 
-int Game::get_random_num() {
+int Game::get_random_num(int min, int max) {
   // random device
   std::random_device rd;
   std::mt19937 gen(rd());
--- src/game.h
+++ src/game.h
@@ -3,10 +3,10 @@
 #include <optional>
 #include <vector>
 
-const static int min = 0;
-const static int max = 10;
+const static int MIN_NUM = 0;
+const static int MAX_NUM = 10;
 
-const static int max_attempts = 3;
+const static int MAX_ATTEMPTS = 3;
 
 class Game {
 public:
@@ -19,7 +19,7 @@ private:
   int m_attempt = 0;
   std::vector<std::optional<int>> m_guesses;
 
-  int get_random_num();
+  int get_random_num(int min, int max);
   void validating_wait();
   void show_guesses();
   void decide(int guess);
